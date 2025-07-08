840652500d601e5dd9c880a609bd8b7fd4fd24c7
Liz Perdomo <129300353+Lizperdomo@users.noreply.github.com>
2025-07-08 13:11:09 -0600

    Update IGetVisualizeAccountInput.cs

--- Learn.Api.BusinessObjects/Interfaces/FinancialStatement/GetVisualizeAccount/IGetVisualizeAccountInput.cs
+++ Learn.Api.BusinessObjects/Interfaces/FinancialStatement/GetVisualizeAccount/IGetVisualizeAccountInput.cs
@@ -1,10 +1,11 @@
-﻿using Learn.Api.Domain.Entities.Dtos;
+using Learn.Api.Domain.Entities.Dtos;
 using Learn.Api.Domain.Entities.Dtos.FinancialStatement;
 
 namespace Learn.Api.Business.Objects.Interfaces.FinancialStatement.VisualizeAccount;
 
 public interface IGetVisualizeAccountInputPort
 {
-    Task<ResponseVisualizeAccount<VisualizeAccountDto>> GetAllFinancialStatementAsync();
+    Task<ResponseVisualizeAccount<VisualizeAccountDto>> GetAllFinancialStatementAsync(bool status);
+
 }
 
