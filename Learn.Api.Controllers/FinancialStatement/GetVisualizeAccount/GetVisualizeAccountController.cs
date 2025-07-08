36836221b5135283450d9bb1d28c64c808623740
Liz Perdomo <129300353+Lizperdomo@users.noreply.github.com>
2025-07-08 13:12:23 -0600

    Update GetVisualizeAccountController.cs

--- Learn.Api.Controllers/FinancialStatement/GetVisualizeAccount/GetVisualizeAccountController.cs
+++ Learn.Api.Controllers/FinancialStatement/GetVisualizeAccount/GetVisualizeAccountController.cs
@@ -1,4 +1,4 @@
-﻿using Learn.Api.Business.Objects.Interfaces.FinancialStatement.VisualizeAccount;
+using Learn.Api.Business.Objects.Interfaces.FinancialStatement.VisualizeAccount;
 using Learn.Api.Domain.Entities.Dtos;
 using Learn.Api.Domain.Entities.Dtos.FinancialStatement;
 
@@ -7,9 +7,9 @@ namespace Learn.Api.Controllers.FinancialStatement.GetVisualizeAccount;
 
 internal class GetVizualizeAccountController(IGetVisualizeAccountInputPort inputPort) : IGetVisualizeAccountController
 {
-    public async Task<ResponseVisualizeAccount<VisualizeAccountDto>> GetAllFinancialStatementAsync()
+    public async Task<ResponseVisualizeAccount<VisualizeAccountDto>> GetAllFinancialStatementAsync(bool status)
     {
-        return await inputPort.GetAllFinancialStatementAsync();
+        return await inputPort.GetAllFinancialStatementAsync(status);
     }
 }
 
